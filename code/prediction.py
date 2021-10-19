2d252bf9979b5b55ce721e1ca1f560d5c07ca8d6
Nazareth <1718110400@utectulancingo.edu.mx>
2021-10-19 06:59:57 +0000

    Predicciones terminadas

---
+++ code/prediction.py
@@ -0,0 +1,11 @@
+#Cargar el modelo 
+import sklearn 
+from joblib import load 
+ 
+model = load("model.joblib") 
+while True: 
+    xs = [] 
+    x = int(input("x:")) 
+    xs.append([x]) 
+    prediction = model.predict(xs) 
+    print(prediction)
